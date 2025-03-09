4269bd3a606ecc0950781557c91bd2a57cf9d9bc
bendalejatin <bendalejatin35@gmail.com>
2025-03-09 20:36:13 +0530

    Website updatehome page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,6 +9,9 @@ import home_bg3 from "./Images/slider3.avif";
 import playstoreimg from "./Images/Google_Playimg.avif";
 import appstoreimg from "./Images/App_Logo_PlayStore.avif";
 
+import bear from "./Images/bear_tree.png";
+import arpuzzlecover from "./Images/arpuzzle_cover.jpg";
+
 const images = [home_bg1, home_bg2, home_bg3];
 
 const Home = () => {
@@ -21,6 +24,12 @@ const Home = () => {
 
     return () => clearInterval(interval);
   }, []);
+
+  const [showVideo, setShowVideo] = useState(false);
+
+  const handlePlayVideo = () => {
+    setShowVideo(true);
+  };
 
   return (
     <div>
@@ -62,13 +71,72 @@ const Home = () => {
               <h3>With AR</h3>
             </div>
           </div>
-          <a href="https://play.google.com/store/apps/details?id=com.DECKidz.AnimalEncyclopediaWithAR" className="download-btn">
+          <a
+            href="https://play.google.com/store/apps/details?id=com.DECKidz.AnimalEncyclopediaWithAR"
+            className="download-btn"
+          >
             <img src={playstoreimg} alt="Google Play Logo" />
             <div>
               <span>GET IT FOR</span>
               <h3>Android</h3>
             </div>
           </a>
+        </div>
+      </div>
+
+      <div className="product-tour">
+        <div className="banner">
+          <h1>Product Tour</h1>
+        </div>
+
+        <div className="section">
+        <div className="ar-image">
+            <img src={bear} alt="Bear under tree" />
+          </div>
+          <h2 className="ar-title">AR Animal Games Function</h2>
+        </div>
+
+        <div className="ar-content">
+          
+
+          <div className="ar-video-container">
+            {/* <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/RIT0GjT8q8Q?si=oL5H5Dea_jvfVEcX"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe> */}
+            <div className="video-wrapper">
+      {!showVideo ? (
+        <div className="cover" onClick={handlePlayVideo}>
+          <img
+            src={arpuzzlecover}
+            alt="Video Thumbnail"
+            className="video-thumbnail"
+          />
+          <div className="play-button">▶</div>
+        </div>
+      ) : (
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/RIT0GjT8q8Q?autoplay=1"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      )}
+    </div>
+          </div>
+        </div>
+        <div className="details-button">
+          <button>Details</button>
         </div>
       </div>
     </div>
